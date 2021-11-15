8dcc2c6de3064ff6237bc1a12f556a94d15b3f03
Mike Mitchell <alchemistikk@gmail.com>
2021-11-15 13:25:33 -0500

    Complete fibonacci method implementation with iteration

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,3 +1,21 @@
 # Using iteration, write a method #fibs which takes a number and 
 # returns an array containing that many numbers from the fibonacci sequence.
 
+def fibs(num)
+  return [0] if num.equal?(0)
+  return [0, 1] if num.equal?(1)
+
+  arr = [0, 1]
+  i = 1
+  (num - 1).times do
+    arr << arr[i - 1] + arr[i]
+    i += 1
+  end
+  arr
+end
+
+p fibs(0) # => [0]
+p fibs(1) # => [0, 1]
+p fibs(2) # => [0, 1, 1]
+p fibs(5) # => [0, 1, 1, 2, 3, 5]
+p fibs(6) # => [0, 1, 1, 2, 3, 5, 8]
