d3246f65676d0d6620c8e0138f777b01dbfae038
Mike Mitchell <alchemistikk@gmail.com>
2021-11-15 14:25:43 -0500

    Complete fibonacci method implementation with recursion

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,4 +1,4 @@
-# Using iteration, write a method #fibs which takes a number and 
+# Using iteration, write a method #fibs which takes a number and
 # returns an array containing that many numbers from the fibonacci sequence.
 
 def fibs(num)
@@ -19,3 +19,19 @@ p fibs(1) # => [0, 1]
 p fibs(2) # => [0, 1, 1]
 p fibs(5) # => [0, 1, 1, 2, 3, 5]
 p fibs(6) # => [0, 1, 1, 2, 3, 5, 8]
+
+# Now write another method #fibs_rec which solves the same problem recursively.
+
+def fibs_rec(num)
+  return [0] if num.equal?(0)
+  return [0, 1] if num.equal?(1)
+
+  arr = fibs_rec(num - 1)
+  arr << arr[-2] + arr[-1]
+end
+
+p fibs_rec(0) # => [0]
+p fibs_rec(1) # => [0, 1]
+p fibs_rec(2) # => [0, 1, 1]
+p fibs_rec(5) # => [0, 1, 1, 2, 3, 5]
+p fibs_rec(6) # => [0, 1, 1, 2, 3, 5, 8]
